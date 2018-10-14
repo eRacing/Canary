f4d3df7f40bb7497745db375dd27e36ef064aa61
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-13 23:36:25 -0400

    Add Signedness enumeration

---
+++ src/main/java/com/polymtl/eracing/cananalyzer/signal/Signedness.java
@@ -0,0 +1,16 @@
+package com.polymtl.eracing.cananalyzer.signal;
+
+/**
+ * This enumeration represents a signedness of a signal.
+ */
+public enum Signedness {
+    /**
+     * A signal that can contain both positive and negative value.
+     */
+    SIGNED,
+
+    /**
+     * A signal that can contain only positive value.
+     */
+    UNSIGNED;
+}
