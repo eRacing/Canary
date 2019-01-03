c03b991cf7904fbe391ab70b05ae9b8c14e6fded
PinkTheRock <rosanne.larocque@polymtl.ca>
2019-01-02 22:30:59 -0500

    squelette JavaFx, avec plus de chair

--- src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/MainGUI.java
+++ src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/MainGUI.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+
+public class MainGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CANary");
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
