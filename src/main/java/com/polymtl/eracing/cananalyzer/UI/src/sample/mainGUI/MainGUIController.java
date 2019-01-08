59e23011d65a6ff1802467392b5c9ecca521436e
PinkTheRock <rosanne.larocque@polymtl.ca>
2019-01-08 13:50:03 -0500

    AnchorPane Version... testing

--- src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUIController.java
+++ src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUIController.java
@@ -3,65 +3,46 @@ package sample.mainGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class MainGUIController implements Initializable {
+public class MainGUIController {
+
+    @FXML
+    private MenuItem newButton;
+
+    @FXML
+    private TreeView<?> dataNavigator;
+
+    @FXML
+    private ListView<?> dataListView;
 
     @FXML
     private MenuBar topMenu;
-    @FXML
-    private MenuItem newMenuItem;
-    @FXML
-    private TreeView<String> dataNavigator;
-    @FXML
-    private ListView<String> dataListView;
+
     @FXML
     private Pane chartView;
+
     @FXML
     private Button newVisualButton;
-    @FXML
-    private Button existingButton;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-
-    }
-
-
-    @FXML
-    void openExistingFile(ActionEvent event) {
-
-        //testing
-        System.out.println("OpenExistingFile...");
-
-    }
 
     @FXML
     void startNewVisualisation(ActionEvent event) throws IOException {
+
         //testing
         System.out.println("Starting new visualisation...");
 
         Pane newRoot = FXMLLoader.load(getClass().getResource("/sample/repertoriesTree/repertoriesTree.fxml"));
 
-        chartView.getChildren().setAll(newRoot);
+
 
         newRoot.prefWidthProperty().bind(chartView.widthProperty());
         newRoot.prefHeightProperty().bind(chartView.heightProperty());
+
 
 
     }
--- src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUILoader.java
+++ src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUILoader.java
@@ -1,18 +1,23 @@
 package sample.mainGUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ListView;
+
 
 public class MainGUILoader extends Application {
 
+    @FXML ListView dataListView;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("/sample/mainGUI/mainGUI.fxml"));
         primaryStage.setTitle("CANary");
         primaryStage.setScene(new Scene(root, 800, 500));
