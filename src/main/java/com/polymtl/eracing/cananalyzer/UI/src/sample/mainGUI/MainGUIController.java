5a2a5157288b074d088dbb51362e21d5dba4ba3d
PinkTheRock <rosanne.larocque@polymtl.ca>
2019-01-14 20:32:23 -0500

    driver selector initialized

--- src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUIController.java
+++ src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/mainGUI/MainGUIController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.TextArea;
@@ -25,10 +26,14 @@ public class MainGUIController implements Initializable {
 
     @FXML private AnchorPane graphCanva;
 
+    @FXML private ComboBox driverButton;
+
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        initDriverButton();
 
         try {
             initCanTable();
@@ -45,6 +50,12 @@ public class MainGUIController implements Initializable {
         graphCanva.getChildren().add(tableWindow);
         tableWindow.prefWidthProperty().bind(graphCanva.widthProperty());
         tableWindow.prefHeightProperty().bind(graphCanva.heightProperty());
+    }
+
+    private void initDriverButton() {
+
+        driverButton.getItems().addAll("PEAK USB", "WIFI SOMEDAY", "3G SOMEDAY", "UNDERGROUD TUNNEL");
+
     }
 
 
@@ -76,6 +87,8 @@ public class MainGUIController implements Initializable {
     void associateButtonClicked(ActionEvent event) {
 
     }
+
+
 
 
 }
