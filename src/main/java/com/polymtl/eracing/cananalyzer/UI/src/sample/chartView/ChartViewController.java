d47bac2343a250ed50f3ddc4ee95df7f507d1006
PinkTheRock <rosanne.larocque@polymtl.ca>
2019-01-14 20:58:23 -0500

    squelette chartView ...

---
+++ src/main/java/com/polymtl/eracing/cananalyzer/UI/src/sample/chartView/ChartViewController.java
@@ -0,0 +1,23 @@
+package sample.chartView;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//TODO
+/* -connect chartView to BackEnd
+*  -add tool Menu for the charts
+*  -make the layout customizable*/
+
+public class ChartViewController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+}
