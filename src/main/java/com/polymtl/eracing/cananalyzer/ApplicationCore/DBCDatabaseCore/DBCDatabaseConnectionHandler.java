508197eb8d4a8c2bde0e37fe1c07b4505f82c9b7
Phil <philbond1997@hotmail.com>
2019-03-14 18:31:27 -0400

    test

--- src/main/java/com/polymtl/eracing/cananalyzer/ApplicationCore/DBCDatabaseCore/DBCDatabaseConnectionHandler.java
+++ src/main/java/com/polymtl/eracing/cananalyzer/ApplicationCore/DBCDatabaseCore/DBCDatabaseConnectionHandler.java
@@ -1,4 +1,22 @@
 package com.polymtl.eracing.cananalyzer.ApplicationCore.DBCDatabaseCore;
 
 public class DBCDatabaseConnectionHandler {
+    // Holds the path to the DBC file currently associated with the application.
+    private String f_DBCFilePath;
+
+    /**
+     * Associates a DBC file to all messages and signals.
+     * @param filePath The absolute path to the DBC file.
+     */
+    public void associateDB(String filePath) {
+
+    }
+
+    /**
+     * Disassociate the currently associated DBC file with the application.
+     */
+    public void disassociateDB() {
+
+    }
+
 }
